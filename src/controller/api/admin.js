c6f27ed70d89b8803d7ae62017acd391a341d054
Dahren Ericsson <89411491+Redcoat21@users.noreply.github.com>
2024-06-08 21:29:39 +0700

    Fixed faker js import issues, import using @faker-js/faker, not faker

--- src/controller/api/admin.js
+++ src/controller/api/admin.js
@@ -1,79 +1,87 @@
 const {
-  validateLoginSchema,
-  validateRegisterSchema,
-} = require("../../validation/api/auth")
-const { getUser } = require("../../service/user")
-const { findProduct, findAllProducts } = require("../../service/product")
-const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = require("../../config")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const { createToken } = require("../../service/token")
-const Joi = require("Joi")
+    validateLoginSchema,
+    validateRegisterSchema,
+} = require("../../validation/api/auth");
+const { getUser } = require("../../service/user");
+const { findProduct, findAllProducts } = require("../../service/product");
+const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = require("../../config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { createToken } = require("../../service/token");
+const Joi = require("joi");
 
 const getAllProduct = async (req, res) => {
-  let { username, password } = req.body
+    let { username, password } = req.body;
 
-  if (username && password) {
-    let findUser = await getUser(username)
+    if (username && password) {
+        let findUser = await getUser(username);
 
-    if (!findUser) {
-      return res.status(400).json({ Message: "User not found!" })
-    }
+        if (!findUser) {
+            return res.status(400).json({ Message: "User not found!" });
+        }
 
-    if (!bcrypt.compareSync(password, findUser.password)) {
-      return res.status(400).json({ Message: "Wrong Password Check again!" })
-    }
+        if (!bcrypt.compareSync(password, findUser.password)) {
+            return res
+                .status(400)
+                .json({ Message: "Wrong Password Check again!" });
+        }
 
-    let output = await findAllProducts()
+        let output = await findAllProducts();
 
-    if (findUser.role == "adm") {
-      return res.status(200).json({ "Products List": output })
+        if (findUser.role == "adm") {
+            return res.status(200).json({ "Products List": output });
+        } else {
+            return res
+                .status(403)
+                .json({ "Foridden Access": "Only Admin can Access" });
+        }
     } else {
-      return res
-        .status(403)
-        .json({ "Foridden Access": "Only Admin can Access" })
+        return res
+            .status(400)
+            .json({ Error: "Username or Password if empty!" });
     }
-  } else {
-    return res.status(400).json({ Error: "Username or Password if empty!" })
-  }
-}
+};
 
 const getProducts = async (req, res) => {
-  let { username, password, id } = req.body
+    let { username, password, id } = req.body;
 
-  if (username && password) {
-    let findUser = await getUser(username)
+    if (username && password) {
+        let findUser = await getUser(username);
 
-    if (!findUser) {
-      return res.status(400).json({ Message: "User not found!" })
-    }
+        if (!findUser) {
+            return res.status(400).json({ Message: "User not found!" });
+        }
 
-    if (!bcrypt.compareSync(password, findUser.password)) {
-      return res.status(400).json({ Message: "Wrong Password Check again!" })
-    }
+        if (!bcrypt.compareSync(password, findUser.password)) {
+            return res
+                .status(400)
+                .json({ Message: "Wrong Password Check again!" });
+        }
 
-    if (!id) {
-      return res.status(400).json({ Error: "ID needed!" })
-    }
+        if (!id) {
+            return res.status(400).json({ Error: "ID needed!" });
+        }
 
-    if (parseInt(id)) {
-      if (findUser.role == "adm") {
-        let output = await findProduct(id)
-        return res.status(200).json({ "Products List": output })
-      } else {
-        return res
-          .status(403)
-          .json({ "Foridden Access": "Only Admin can Access" })
-      }
+        if (parseInt(id)) {
+            if (findUser.role == "adm") {
+                let output = await findProduct(id);
+                return res.status(200).json({ "Products List": output });
+            } else {
+                return res
+                    .status(403)
+                    .json({ "Foridden Access": "Only Admin can Access" });
+            }
+        } else {
+            return res.status(403).json({ Error: "Id Invalid!" });
+        }
     } else {
-      return res.status(403).json({ Error: "Id Invalid!" })
+        return res
+            .status(400)
+            .json({ Error: "Username or Password if empty!" });
     }
-  } else {
-    return res.status(400).json({ Error: "Username or Password if empty!" })
-  }
-}
+};
 
 module.exports = {
-  getAllProduct,
-  getProducts,
-}
+    getAllProduct,
+    getProducts,
+};
--- src/controller/api/seller.js
+++ src/controller/api/seller.js
@@ -1,68 +1,72 @@
 const {
-  validateLoginSchema,
-  validateRegisterSchema,
-} = require("../../validation/api/auth")
+    validateLoginSchema,
+    validateRegisterSchema,
+} = require("../../validation/api/auth");
 const {
-  getSellerProduct,
-  removeproduct,
-  checkvalid,
-} = require("../../service/product")
-const { getUser } = require("../../service/user")
-const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = require("../../config")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const { createToken } = require("../../service/token")
-const faker = require("faker")
+    getSellerProduct,
+    removeproduct,
+    checkvalid,
+} = require("../../service/product");
+const { getUser } = require("../../service/user");
+const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = require("../../config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { createToken } = require("../../service/token");
+const faker = require("@faker-js/faker");
 
 const sellerProducts = async (req, res) => {
-  let { username, password, id } = req.body
+    let { username, password, id } = req.body;
 
-  if (username && password) {
-    let findUser = await getUser(username)
+    if (username && password) {
+        let findUser = await getUser(username);
 
-    if (!findUser) {
-      return res.status(400).json({ Message: "User not found!" })
-    }
+        if (!findUser) {
+            return res.status(400).json({ Message: "User not found!" });
+        }
 
-    if (!bcrypt.compareSync(password, findUser.password)) {
-      return res.status(400).json({ Message: "Wrong Password Check again!" })
-    }
+        if (!bcrypt.compareSync(password, findUser.password)) {
+            return res
+                .status(400)
+                .json({ Message: "Wrong Password Check again!" });
+        }
 
-    if (!id) {
-      return res.status(400).json({ Error: "ID needed!" })
-    }
+        if (!id) {
+            return res.status(400).json({ Error: "ID needed!" });
+        }
 
-    if (parseInt(id)) {
-      if (findUser.role == "slr") {
-        let output = await getSellerProduct(username)
-        return res.status(200).json({ "Products List": output })
-      } else {
-        return res
-          .status(403)
-          .json({ "Foridden Access": "Only Seller can Access" })
-      }
+        if (parseInt(id)) {
+            if (findUser.role == "slr") {
+                let output = await getSellerProduct(username);
+                return res.status(200).json({ "Products List": output });
+            } else {
+                return res
+                    .status(403)
+                    .json({ "Foridden Access": "Only Seller can Access" });
+            }
+        } else {
+            return res.status(403).json({ Error: "Id Invalid!" });
+        }
     } else {
-      return res.status(403).json({ Error: "Id Invalid!" })
+        return res
+            .status(400)
+            .json({ Error: "Username or Password if empty!" });
     }
-  } else {
-    return res.status(400).json({ Error: "Username or Password if empty!" })
-  }
-}
+};
 
 const AddsellerProduct = async (req, res) => {
-  let {} = req.body
+    let {} = req.body;
 
-  const id = faker.commerce.isbn({
-    separator: "-",
-  })
+    const id = faker.commerce.isbn({
+        separator: "-",
+    });
 
-  return res.status(200).json(id)
-}
+    return res.status(200).json(id);
+};
 
-const deletetsellerproduct = async (req, res) => {}
+const deletetsellerproduct = async (req, res) => {};
 
 module.exports = {
-  sellerProducts,
-  deletetsellerproduct,
-  AddsellerProduct,
-}
+    sellerProducts,
+    deletetsellerproduct,
+    AddsellerProduct,
+};
