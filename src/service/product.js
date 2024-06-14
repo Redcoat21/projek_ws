8343a822dea05c39e03e45223391b603a3f09b2d
Dahren Ericsson <89411491+Redcoat21@users.noreply.github.com>
2024-06-15 02:08:10 +0700

    Added service to create product

--- src/service/product.js
+++ src/service/product.js
@@ -1,9 +1,26 @@
 const { Product } = require("../model");
+const { randomUUID } = require('crypto');
 
 const getProduct = async (id) => {
-    return Product.findByPk(id);
+    return await Product.findByPk(id);
+}
+
+const addProduct = async (productData) => {
+    const { name, description, seller, price } = productData;
+    const id = randomUUID();
+
+    await Product.create({
+        id: id,
+        name: name,
+        description: description,
+        seller: seller,
+        price: price,
+        createdAt: luxon.DateTime.now()
+    });
+
 }
 
 module.exports = {
-    getProduct
+    getProduct,
+    addProduct
 }
